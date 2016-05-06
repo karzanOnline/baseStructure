bc0568f6cb7675d578e8ed82d5d4b34379514b23
caozheng_home <170579160@qq.com>
2016-05-06 18:00:06 +0800

    my first

---
+++ src/set.js
@@ -0,0 +1,103 @@
+
+/*
+* 创建一个集合的构造函数
+* set数据类型
+* by caozheng
+* */
+function setFuc(){
+    /*
+    * 用数组模拟集合*/
+    var items = {};
+    /*
+    * 检测集合中是否有某元素*/
+    this.has = function(value){
+        return items.hasOwnProperty(value)
+    };
+    /*
+    * 向集合中添加不重复的元素*/
+    this.add = function(value){
+        /*检测不存在*/
+        if(!this.has(value)){
+            items[value] = value;
+            return true
+        }
+        return false
+    };
+    /*从集合中去除某元素*/
+    this.remove = function(value){
+        /*检测存在*/
+        if(this.has(value)){
+            delete items[value];
+            return true
+        }
+        return false
+    };
+    /*清空集合*/
+    this.clear = function(){
+        var arr = Object.keys(items)||[];
+        /*第一种方法*/
+        arr.forEach(function(value,index){
+            delete items[value]
+        });
+        /*第二种方法
+        *  -.-直接清空 爱咋咋地
+        * */
+        //items ={}
+    };
+    /*集合大小*/
+    this.size =  function(){
+        var arr = Object.keys(items)||[];
+        return arr.length
+    };
+    /*获取集合所有属性*/
+    this.keys = function(){
+        return Object.keys(items)
+    };
+    /*获取集合所有的值*/
+    this.values = function(){
+        var arr = Object.keys(items).map(function(val){
+            return items[val]
+        });
+        return arr
+    };
+    /*获取两个集合的并集*/
+    this.union = function(otherSet){
+        var result = this.keys();
+        var tempArr = Object.keys(otherSet);
+        tempArr.forEach(function(value,index){
+            result.add(tempArr[value])
+        })
+    };
+    /*获取两个集合的交集*/
+    this.intersection = function(otherSet){
+        var Arryx = this.keys();
+        var ArryY = Object.keys(otherSet);
+        var tempArray = [];
+        var temp = new Set();
+        /*赋值给最大的数组*/
+        (Arryx.size()>=ArryY.size())?(tempArray = Arryx): (tempArray = ArryY);
+        tempArray.forEach(function(val){
+            if(otherSet.has(val)){
+                temp.add(val);
+            }
+        })
+    };
+    /*获取两个集合的差集*/
+    this.difference = function(otherSet){
+        var selfArr = this.keys();
+        var otherArr = Object.keys(otherSet);
+        var tempSet = new Set();
+        /*赋值给最大的数组*/
+        (Arryx.size()>=ArryY.size())?(tempArray = Arryx): (tempArray = ArryY);
+        tempArray.forEach(function(val){
+            if(!otherSet.has(val)){
+                tempSet.add(val);
+            }
+        })
+    };
+    /*判断传入集合是否为当前集合的子集*/
+    this.subset = function(otherSet){
+        //todo waite
+    }
+
+}
