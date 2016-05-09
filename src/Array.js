76697e044f26d9828a00a7bb6aa855d598e09b69
caozheng_home <170579160@qq.com>
2016-05-09 15:28:55 +0800

    '添加数组扁平化'

---
+++ src/Array.js
@@ -0,0 +1,49 @@
+/**
+ * Created by caozheng on 2016/5/9.
+ */
+
+/*二维数组扁平化*/
+Array.prototype.flattening = function(){
+    var _this = this;
+    var tempArray = [];
+    for(var i =0;i<_this.length;i++){
+        tempArray = tempArray.concat(_this[i])
+    }
+    return tempArray
+};
+/*多维数组扁平化*/
+Array.prototype.flattenings = function(){
+    var _this = this;
+    return recursion(_this);
+};
+
+function recursion (arr){
+    var tempArray = [];
+    if(arr instanceof Array){
+        for(var i =0;i<arr.length;i++){
+            if(arr instanceof Array){
+                tempArray = recursionConcat(tempArray,arr[i])
+            }else{
+                tempArray = tempArray.concat(arr[i]);
+            }
+        }
+        return tempArray
+    }else{
+        return tempArray
+    }
+}
+function recursionConcat (tempArray,array){
+    if(array instanceof Array){
+        /*当前循环对象为数组*/
+        for(var i = 0; i<array.length;i++){
+            if(array[i] instanceof Array){
+                tempArray = recursionConcat(tempArray,array[i]);
+            }else{
+                tempArray = tempArray.concat(array[i]);
+            }
+        }
+        return tempArray
+    }else{
+        return tempArray
+    }
+}
