6be4bca392fb107839ddc8f4cfd53dd2b45b36f0
caozheng_home <170579160@qq.com>
2016-05-13 15:31:14 +0800

    添加数组 深度拷贝

--- src/Array.js
+++ src/Array.js
@@ -31,3 +31,30 @@ function recursion (tempArray,arr){
     }
     return tempArray
 }
+/*数组深度拷贝*/
+Array.prototype.deepCopy = function(){
+    /*参数数组化*/
+    var _this = this;
+    var resultArr = [];
+    var index = _this.length;
+    var args = [].slice.call(arguments);
+    var newArray = [];
+    /*数组本身值copy*/
+    for(var i =0;i<index;i++){
+        resultArr[i] = _this[i];
+    }
+    args = args.flattenings();
+    for(var j = 0;j<args.length;j++){
+        resultArr[index+j] = args[j];
+    }
+    /*数组去重*/
+    for(var z=resultArr.length;z>=0;i--){
+        /*标记是否遇到重复的*/
+        for(var m =resultArr.length-1;m>=0;i--){
+            if(resultArr[z]==resultArr[m]){
+                resultArr.splice(m,1);
+            }
+        }
+    }
+    return resultArr
+};
