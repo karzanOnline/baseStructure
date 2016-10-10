05b16cccf20e787d6fd412d2b221de299ec77138
karzanOnline <170579160@qq.com>
2016-10-10 14:45:25 +0800

    添加arraySort

--- README.md
+++ README.md
@@ -1,4 +1,10 @@
 # baseStructure
+ 
+ set.js
  ES6中加入新的数据结构Set 在ES5中模拟Set实现 Set.js
 
+ Array.js
  ES5中实现多维数组扁平化  数组值拷贝
+
+ arraySort.js
+ 关于算法的排序和去重的实现
--- src/Array.js
+++ src/Array.js
@@ -2,75 +2,93 @@
  * Created by caozheng on 2016/5/9.
  */
 
-/*二维数组扁平化*/
-Array.prototype.flattening = function(){
-    var _this = this;
-    var tempArray = [];
-    for(var i =0;i<_this.length;i++){
-        tempArray = tempArray.concat(_this[i])
+/* 二维数组扁平化*/
+Array.prototype.flattening = function () {
+
+    let _this = this,
+        tempArray = [];
+
+    for (let i = 0; i < _this.length; i++) {
+
+        tempArray = tempArray.concat(_this[i]);
     }
-    return tempArray
+
+    return tempArray;
 };
-/*多维数组扁平化*/
-Array.prototype.flattenings = function(){
-    var _this = this;
-    var tempArray = [];
-    return recursion(tempArray,_this);
+
+/* 多维数组扁平化*/
+Array.prototype.flattenings = function () {
+
+    let _this = this,
+        tempArray = [];
+
+    return recursion(tempArray, _this);
 };
 /**/
-function recursion (tempArray,arr){
-    //TODO后期性能优化
-    if(arr instanceof Array){
-        for(var i =0;i<arr.length;i++){
-            if(arr[i] instanceof Array){
-                tempArray = recursion(tempArray,arr[i])
-            }else{
-                tempArray = tempArray.concat(arr[i]);
-            }
+
+function recursion(tempArray, arr) { 
+
+    // TODO后期性能优化
+    if (arr instanceof Array) {
+        for (let i = 0; i < arr.length; i++) {
+
+            arr[i] instanceof Array?(tempArray = recursion(tempArray, arr[i])):
+                (tempArray = tempArray.concat(arr[i]));     
         }
     }
-    return tempArray
+
+    return tempArray;
 }
-/*数组深度拷贝*/
-//数组扁平化+去重
-Array.prototype.deepCopy = function(){
-    /*参数数组化*/
-    var _this = this;
-    var resultArr = [];
-    var index = _this.length;
-    var args = [].slice.call(arguments);
-    /*数组本身值copy*/
-    for(var i =0;i<index;i++){
+
+
+/* 数组深度拷贝*/
+// 数组扁平化+去重
+Array.prototype.deepCopy = function () {
+
+    /* 参数数组化*/
+    let _this = this;
+    let resultArr = [];
+    let index = _this.length;
+    let args = [].slice.call(arguments);
+
+    /* 数组本身值copy*/
+    for (let i = 0; i < index; i++) {
+
         resultArr[i] = _this[i];
     }
+
     args = args.flattenings();
-    for(var j = 0;j<args.length;j++){
-        resultArr[index+j] = args[j];
+
+    for (let j = 0; j < args.length; j++) {
+
+        resultArr[index + j] = args[j];
     }
-    /*数组去重*/
-    for(var z=resultArr.length-1;z>=0;z--){
-        for(var m =z-1;m>=0;m--){
-            if(resultArr[z]==resultArr[m]){
-                resultArr.splice(m,1);
-            }
+
+    /* 数组去重*/
+    for (let z = resultArr.length - 1; z >= 0; z--) {
+
+        for (let m = z - 1; m >= 0; m--) {
+            
+            resultArr[z] == resultArr[m]&&resultArr.splice(m, 1);
         }
     }
-    return resultArr
+    return resultArr;
 };
-//不限层级数组深度拷贝 尾递归
-//重新开辟一块内存
-Array.prototype.extendArray = function(arr){
-    return function circulation (arr){
-        var newArr = [];
-        if (arr instanceof Array){
-            for (var i= 0;i<arr.length;i++){
-                if (arr[i] instanceof  Array){
-                    newArr[i] = circulation(arr[i])
-                }else {
-                    newArr[i] = arr[i]
-                }
+
+// 不限层级数组深度拷贝
+// 重新开辟一块内存
+Array.prototype.extendArray = function (arr) {
+    
+    return function circulation(arr) {
+
+        let newArr = [];
+        if (arr instanceof Array) {
+            for (let i = 0; i < arr.length; i++) {
+                
+                arr[i] instanceof Array?(newArr[i] = circulation(arr[i])):(newArr[i] = arr[i])
             }
-            return newArr
+            return newArr;
         }
-    }
-}
+    };
+};
+
